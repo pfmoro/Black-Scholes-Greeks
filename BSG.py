c36b646b531eba5465a7bd815ecaeb9e94bcd4ec
Pedro Moro <47028923+pfmoro@users.noreply.github.com>
2020-03-15 20:41:17 -0300

    Update BSG.py
    Small Fixes and change of math dependency for numpy to increase calculation speed

--- BSG.py
+++ BSG.py
@@ -10,33 +10,34 @@
 #x=variable
 
 import math
+import numpy as np
 from scipy.stats import norm
 
 def D1(S0, X, r, Sigma, T):
   #a variable that is usedin all calculations
-  d1=math.log((S0/X)+(r+(Sigma^2)/2)*T/(Sigma*math.sqrt(T)))
+  d1=((np.log(S0/X)+(r+(Sigma**2)/2)*T)/(Sigma*(T**0.5)))
   return d1
 
 def D2(S0, X, r, Sigma, T):
   #a variable that is usedin all calculations
-  d2=math.log((S0/X)+(r-(Sigma^2)/2)*T/(Sigma*sqrt(T)))
+  d2=((np.log(S0/X)+(r-(Sigma**2)/2)*T)/(Sigma*(T**0.5)))
   return d2
 
-def Nline(x)
+def Nline(x):
  #Derivative of normal cumulative distribution function
- NDer=(1/math.sqrt(2*pi()))*exp(-(x^2)/2)
-return NDer
+ NDer=(1/math.np.sqrt(2*np.pi()))*np.exp(-(x^2)/2)
+ return NDer
 
-def Delta(S0, X, r, Sigma, T,type="Call"):
+def Delta(S0, X, r, Sigma, T,Type="Call"):
   #variation of option price per unit of variation of stock price
   d1=D1(S0, X, r, Sigma, T)
-  if type="Call":
+  if Type=="Call":
     myDelta=norm.cdf(d1)
   else:
     myDelta=norm.cdf(d1)-1
   return myDelta
 
-def Gamma(S0, X, r, Sigma, T,type="Call"):
+def Gamma(S0, X, r, Sigma, T,Type="Call"):
    #variation of option price per unit of variation of delta
   d1=D1(S0, X, r, Sigma, T)
   NL=Nline(d1)
@@ -44,22 +45,22 @@ def Gamma(S0, X, r, Sigma, T,type="Call"):
   myGamma=NL/(S0*Sigma*math.sqrt(T))  
   return myGamma
 
-def Theta(S0, X, r, Sigma, T,type="Call"):
+def Theta(S0, X, r, Sigma, T,Type="Call"):
   #variation of option price per unit of time variation
   d1=D1(S0, X, r, Sigma, T)
   d2=D2(S0, X, r, Sigma, T)
   NL=Nline(d1)
   
-  myTheta=S0*NL*Sigma -r*X*exp(-r*T)*norm.cdf(d2)
+  myTheta=S0*NL*Sigma -r*X*np.exp(-r*T)*norm.cdf(d2)
   return myTheta
 
-def BS(S0, X, r, Sigma, T,type="Call")):
+def BS(S0, X, r, Sigma, T,Type="Call"):
   #returns option price
   d1=D1(S0, X, r, Sigma, T)
   d2=D2(S0, X, r, Sigma, T)
-  PV=X*exp(-r*(T))
-  if type="Call":
+  PV=X*np.exp(-r*(T))
+  if Type=="Call":
     price=norm.cdf(d1)*S0-norm.cdf(d2)*PV
   else:
-    price=PV-S0+norm.cdf(d1)*S0-norm.cdf(d2)*PV
+    price=PV*norm.cdf(-d2)-S0*norm.cdf(-d1)
   return price
